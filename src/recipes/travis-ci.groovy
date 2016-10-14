1b37ed075d8ebecc8cbf4135c322689f6656543d
Cedric Champeau <cedric.champeau@gmail.com>
2016-10-14 21:13:55 +0200

    Add Travis build URL

--- src/recipes/travis-ci.groovy
+++ src/recipes/travis-ci.groovy
@@ -2,6 +2,7 @@ def env = System.getenv()
 
 if (env.CI && env.TRAVIS && env.BUILD_SERVER_URL && env.BUILD_TYPE_ID) {
     buildScan.tag 'CI'
+    buildScan.link 'Travis Build', "https://travis-ci.org/${env.TRAVIS_REPO_SLUG}/builds/${env.TRAVIS_JOB_ID}"
     buildScan.value 'Travis Branch', env.TRAVIS_BRANCH
     buildScan.value 'Travis Commit', env.TRAVIS_COMMIT
     buildScan.value 'Travis Pull Request', env.TRAVIS_PULL_REQUEST
