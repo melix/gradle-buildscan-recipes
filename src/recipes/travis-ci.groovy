88d0e0336057e030e4d9fb416a570e3cd9ee36c2
Cedric Champeau <cedric.champeau@gmail.com>
2016-10-14 21:35:34 +0200

    Fix Travis recognition

--- src/recipes/travis-ci.groovy
+++ src/recipes/travis-ci.groovy
@@ -1,6 +1,6 @@
 def env = System.getenv()
 
-if (env.CI && env.TRAVIS && env.BUILD_SERVER_URL && env.BUILD_TYPE_ID) {
+if (env.CI && env.TRAVIS) {
     buildScan.tag 'CI'
     buildScan.link 'Travis Build', "https://travis-ci.org/${env.TRAVIS_REPO_SLUG}/builds/${env.TRAVIS_JOB_ID}"
     buildScan.value 'Travis Branch', env.TRAVIS_BRANCH
