569bd821d410527bcf86ada93afad45e4d16745b
Cedric Champeau <cedric.champeau@gmail.com>
2016-10-14 21:18:55 +0200

    Remove extension method

--- src/main/groovy/me/champeau/gradle/buildscans/BuildScanExtensionModule.groovy
+++ src/main/groovy/me/champeau/gradle/buildscans/BuildScanExtensionModule.groovy
@@ -1,10 +1,9 @@
 package me.champeau.gradle.buildscans
 
 import groovy.transform.CompileStatic
-import com.gradle.scan.plugin.BuildScanExtension
 
 @CompileStatic
 class BuildScanExtensionModule {
-   static void recipe(BuildScanExtension self, String recipe) { println "Recipe: $recipe" }
+
 }
 
