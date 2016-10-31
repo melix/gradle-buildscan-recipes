3690856e36010255f54b7dfe3d01641f805a14e2
Cedric Champeau <cedric.champeau@gmail.com>
2016-10-31 19:13:39 +0100

    Measure directory size at the beginning and at the end of the build
    The `disk-usage` recipe will now measure the size of directories at
    the beginning then at the end of the build, and generate a diff
    output (like `45MB (+5MB)`).

--- src/recipes/disk-usage.groovy
+++ src/recipes/disk-usage.groovy
@@ -4,19 +4,77 @@
 
 import org.gradle.internal.os.OperatingSystem // remember kids, depending on internal API is WRONG!
 
-String diskUsage(dir) {
-   ['du', '-hs' , dir].execute().text.trim().split('\\s')[0]
-}
-
 def os = OperatingSystem.current()
 if (os.unix || os.macOsX) {
    def home = System.getProperty("user.home")
    def wrapperCount = new File("$home/.gradle/wrapper/dists").listFiles().size()
    def projectDir = gradle.rootProject.projectDir
 
-   ['caches': 'Artifact Cache', 'task-cache': 'Task Output Cache', 'wrapper': "Wrappers ($wrapperCount versions)"].each { dir ->
-       buildScan.value "Disk usage ($dir.value)", diskUsage("$home/.gradle/$dir.key")
+   List<Measurement> measurements = []
+
+   def directoriesToMeasure = ['caches': 'Artifact Cache', 'task-cache': 'Task Output Cache', 'wrapper': "Wrappers ($wrapperCount versions)"]
+
+   directoriesToMeasure.each { dir ->
+      File dirKey = gradle.rootProject.file("$home/.gradle/$dir.key")
+      measurements << new Measurement(dirKey, "Disk usage ($dir.value)")
    }
-   buildScan.value "Disk usage (Project directory)", """${diskUsage(projectDir)} (incl. ${diskUsage(gradle.rootProject.file("$projectDir/.gradle"))} .gradle)"""
+   def projectDirMeasurement = new Measurement(projectDir, 'Disk usage (Project)')
+   def projectDirDotGradle = new Measurement(gradle.rootProject.file("$projectDir/.gradle"), 'Disk usage (Project .gradle)')
+   measurements << projectDirMeasurement
+   measurements << projectDirDotGradle
+
+   buildScan.buildFinished {
+      measurements*.update()
+      projectDirMeasurement.initialSize -= projectDirDotGradle.initialSize
+      projectDirMeasurement.buildFinishedSize -= projectDirDotGradle.buildFinishedSize
+      measurements.each { measurement ->
+         buildScan.value measurement.label, measurement.formattedSize
+      }
+   }
 }
 
+class Measurement {
+   final File dir
+   final String label
+   long initialSize
+   long buildFinishedSize
+
+   Measurement(File dir, String label) {
+      this.dir = dir
+      this.label = label
+      this.initialSize = diskUsage(dir.absolutePath)
+   }
+
+   long getDelta() {
+      buildFinishedSize - initialSize
+   }
+
+   void update() {
+      this.buildFinishedSize = diskUsage(dir.absolutePath)
+   }
+
+   private static long diskUsage(String dir) {
+      Long.valueOf(['du', '-ks' , dir].execute().text.trim().split('\\s')[0]?:'0')
+   }
+
+   private static String format(long amount) {
+      if (amount<1024) {
+         return "${amount}kB"
+      }
+      amount /= 1024
+      if (amount<1024) {
+         return "${amount}MB"
+      }
+      amount /= 1024
+      if (amount<1024) {
+         return "${amount}GB"
+      }
+      amount /= 1024
+      return "${amount}TB"
+   }
+
+   public String getFormattedSize() {
+      String deltaStr = delta==0?'':" (${delta<0?'':'+'}${format(delta)})"
+      "${format(buildFinishedSize)}$deltaStr"
+   }
+}
