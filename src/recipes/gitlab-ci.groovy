27830dcab43eeff53cfad100aefd2ceb0158cc73
gimoh <gimoh@bitmessage.ch>
2017-12-17 00:42:42 +0000

    Add support for GitLab CI
    If build is being executed on
    [GitLab CI](https://docs.gitlab.com/ce/ci/README.html) a number of
    values based on
    [variables](https://docs.gitlab.com/ce/ci/variables/README.html) GitLab
    CI exposes will be automatically added to the build scan.

---
+++ src/recipes/gitlab-ci.groovy
@@ -0,0 +1,37 @@
+def env = System.getenv()
+
+if (!(env.CI && env.GITLAB_CI)) {
+    return
+}
+
+buildScan {
+    tag 'CI'
+    link 'GitLab CI Job', "${env.CI_PROJECT_URL}/-/jobs/${env.CI_JOB_ID}"
+    link 'GitLab CI Pipeline', "${env.CI_PROJECT_URL}/pipelines/${env.CI_PIPELINE_ID}"
+
+    value 'GitLab CI Project Name', env.CI_PROJECT_NAME
+    value 'GitLab CI Project Namespace', env.CI_PROJECT_NAMESPACE
+    value 'GitLab CI Project Path', env.CI_PROJECT_PATH
+    value 'GitLab CI Project URL', env.CI_PROJECT_URL
+    value 'GitLab CI Built Branch', env.CI_COMMIT_REF_NAME
+    value 'GitLab CI Built Commit', env.CI_COMMIT_SHA
+    if (env.CI_COMMIT_TAG) {
+        value 'GitLab CI Tag', env.CI_COMMIT_TAG
+    }
+    value 'GitLab CI Job Started Manually', env.CI_JOB_MANUAL
+    value 'GitLab CI Job Name', env.CI_JOB_NAME
+    value 'GitLab CI Job Stage', env.CI_JOB_STAGE
+    value 'GitLab CI Pipeline Source', env.CI_PIPELINE_SOURCE
+    value 'GitLab CI Environment', env.CI_ENVIRONMENT_NAME
+    value 'GitLab CI Environment URL', env.CI_ENVIRONMENT_URL
+    value 'GitLab CI Runner ID', env.CI_RUNNER_ID
+    value 'GitLab CI Runner Desc', env.CI_RUNNER_DESCRIPTION
+    value 'GitLab CI Runner Tags', env.CI_RUNNER_TAGS
+    value 'GitLab CI Server Name', env.CI_SERVER_NAME
+    value 'GitLab CI Server Revision', env.CI_SERVER_REVISION
+    value 'GitLab CI Server Version', env.CI_SERVER_VERSION
+    value 'GitLab CI User ID', env.GITLAB_USER_ID
+    value 'GitLab CI User Email', env.GITLAB_USER_EMAIL
+    value 'GitLab CI User Login', env.GITLAB_USER_LOGIN
+    value 'GitLab CI User Name', env.GITLAB_USER_NAME
+}
